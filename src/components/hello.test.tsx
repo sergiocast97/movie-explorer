f5d0e2c5a97143923d0caab7b2fe331c541b1e09
Sergio <sergiocast97@gmail.com>
2025-04-27 17:38:02 +0100

    fix: hello component case import

--- src/components/hello.test.tsx
+++ src/components/hello.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Hello from './Hello'
+import Hello from './hello'
 
 describe('App tests', () => {
   it('should render the title', () => {
