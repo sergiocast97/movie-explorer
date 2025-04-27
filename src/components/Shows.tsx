8377e6f7cc544684553fa26067f8d6259571c9c5
Sergio <sergiocast97@gmail.com>
2025-04-27 17:35:23 +0100

    feat: paginated shows fetch

--- src/components/Shows.tsx
+++ src/components/Shows.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react'
 import { useShows } from '../hooks/useShows'
+import type { Show } from '../hooks/useShows'
 
 export default function Shows() {
-  const { data: shows, isLoading, isError } = useShows()
+  // Offset for Pagination
+  const [offset, setOffset] = useState(0)
+  // Pagination limit
+  const limit = 20
+  // Fetch shows through hook
+  const {
+    data: shows,
+    isLoading,
+    isError,
+    isFetching,
+  } = useShows(offset, limit)
 
-  if (isLoading) {
+  const [allShows, setAllShows] = useState<Array<Show>>([]) // Shown shows
+
+  // Add new shows to existing list
+  useEffect(() => {
+    if (shows && !isFetching) {
+      setAllShows((prev) => [...prev, ...shows])
+    }
+  }, [shows])
+
+  // Loading initial shows
+  if (isLoading && offset === 0) {
     return <div>Loading...</div>
   }
 
+  // Display error
   if (isError) {
     return <div>Error fetching shows.</div>
   }
@@ -15,10 +38,18 @@ export default function Shows() {
     <>
       <div>
         <ul>
-          {shows.map((show: any) => (
+          {allShows.map((show: any) => (
             <li key={show.id}>{show.name}</li>
           ))}
         </ul>
+      </div>
+      <div>
+        <button
+          onClick={() => setOffset((prev) => prev + limit)}
+          disabled={isFetching}
+        >
+          {isFetching ? 'Loading...' : 'Show more'}
+        </button>
       </div>
     </>
   )
--- src/hooks/useShows.ts
+++ src/hooks/useShows.ts
@@ -1,16 +1,37 @@
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 
-const fetchShows = async () => {
-  const response = await fetch('https://api.tvmaze.com/shows')
-  if (!response.ok) {
-    throw new Error('Network response was not ok')
-  }
-  return response.json()
+export interface Show {
+  id: number
+  name: string
+  summary: string
 }
 
-export const useShows = () => {
+/**
+ * Function to fetch a specicic number of shows from the TVmaze API
+ * @param offset First show
+ * @param limit Last show
+ * @returns List of shows
+ */
+const fetchShows = async (offset: number, limit: number) => {
+  const response = await fetch('https://api.tvmaze.com/shows')
+  // Handling errors
+  if (!response.ok) throw new Error('Network response was not ok')
+  // First, get all shows
+  const allShows = (await response.json()) as Array<Show>
+  // Return a determined number of shows
+  return allShows.slice(offset, offset + limit)
+}
+
+/**
+ * Hook to fetch a list of shows from TvMaze
+ * @param offset First show
+ * @param limit Last show
+ * @returns Query object
+ */
+export const useShows = (offset: number, limit: number) => {
   return useQuery({
-    queryKey: ['shows'],
-    queryFn: fetchShows,
+    queryKey: ['shows', offset, limit],
+    queryFn: () => fetchShows(offset, limit),
+    placeholderData: keepPreviousData,
   })
 }
