b4c28fa5331c4f13ab252945158b77d616943b37
Sergio <sergiocast97@gmail.com>
2025-04-28 20:50:44 +0100

    feat: properly defined show type

--- src/types/show.ts
+++ src/types/show.ts
@@ -1,8 +1,25 @@
+/**
+ * Show object interface
+ * Note: These are not all the fields, just the ones used
+ * @see https://api.tvmaze.com/shows/:id
+ */
 export interface Show {
-  id: number
-  name: string
-  summary: string
-  image: {
-    medium: string
+  id: number // Show ID on TV Maze
+  url: string // Page URL
+  name: string // Show name
+  type: string // Type (Scripted, Animated, Reality...)
+  language: string // Original language
+  genres: Array<string> // List of genres
+  status: string // Status (Running, Ended...)
+  premiered: string // Premiere date (YYYY-MM-DD)
+  ended: string // End date (YYYY-MM-DD)
+  officialSite: string // Official site URL
+  rating: {
+    average: number // Average rating
   }
+  image: {
+    medium: string // Preview
+    original: string // Original
+  }
+  summary: string // Summary in HTML
 }
