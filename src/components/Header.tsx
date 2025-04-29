3bcc7960aedf7c73c7c344f803eabd26858bdd65
Sergio <sergiocast97@gmail.com>
2025-04-29 01:48:06 +0100

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 import { Link } from '@tanstack/react-router'
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
+  const items = [
+    { name: 'Home', url: '/' },
+    { name: 'Favorites', url: '/favorites' },
+  ]
 
-        <div className="">
-          <Link to="/favorites">Favorites</Link>
+  return (
+    <header className="py-6 lg:pt-12">
+      <nav className="flex items-center justify-between gap-8">
+        <div></div>
+
+        <div className="items-baseline justify-end gap-8 lg:flex lg:gap-12 flex gap-x-6 gap-y-4 md:gap-x-8">
+          {items.map((item) => (
+            <Link
+              className="text-light-darkest dark:text-dark-white text-base font-semibold md:text-lg"
+              to={item.url}
+            >
+              {item.name}
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
